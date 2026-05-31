0ddbfd7ab1a9aa1df86a82bcababa0a9245a1760
Natalia Suárez <natalias@fing.edu.uy>
2026-05-31 16:34:12 -0300

    Update button classes for tab navigation

--- main.py
+++ main.py
@@ -147,10 +147,10 @@ pag_web_completa = f"""<!DOCTYPE html>
     <div class="container">
         <ul class="nav nav-tabs nav-tabs-custom mb-4" id="portalTabs" role="tablist">
            <li class="nav-item" role="presentation">
-                <button class="nav-link" id="mapa-tab" data-bs-toggle="tab" data-bs-target="#mapa-content" type="button" role="tab"><i class="fa-solid fa-map-location-dot me-2"></i>Mapa de Registros</button>
+                <button class="nav-link active" id="mapa-tab" data-bs-toggle="tab" data-bs-target="#mapa-content" type="button" role="tab"><i class="fa-solid fa-map-location-dot me-2"></i>Mapa de Registros</button>
             </li>
             <li class="nav-item" role="presentation">
-                <button class="nav-link active" id="info-tab" data-bs-toggle="tab" data-bs-target="#info-content" type="button" role="tab"><i class="fa-solid fa-book-medical me-2"></i>Información Técnica</button>
+                <button class="nav-link" id="info-tab" data-bs-toggle="tab" data-bs-target="#info-content" type="button" role="tab"><i class="fa-solid fa-book-medical me-2"></i>Información Técnica</button>
             </li>
             <li class="nav-item" role="presentation">
                 <button class="nav-link" id="metro-tab" data-bs-toggle="tab" data-bs-target="#metro-content" type="button" role="tab"><i class="fa-solid fa-gauge me-2"></i>Metrología Legal</button>
